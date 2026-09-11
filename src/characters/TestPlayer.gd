77d8971400c1097521073dde190399dfa19369dd
paruparo <togami.playz2905@gmail.com>
2026-09-11 20:10:13 +0800

    feat: add sprite to TestPlayer.gd - add flip method

--- src/characters/TestPlayer.gd
+++ src/characters/TestPlayer.gd
@@ -3,8 +3,20 @@ extends CharacterBody2D
 class_name TestPlayer
 
 
+var sprite_direction : int = 1
+
+
 @onready var state_machine : StateMachine = $StateMachine
+@onready var sprite : AnimatedSprite2D = $AnimatedSprite2D
 
 
 func _physics_process(delta: float) -> void:
 	move_and_slide()
+	
+	
+func flip_x(direction: int) -> void:
+	if direction == 0:
+		return
+	
+	sprite_direction = signi(direction)
+	sprite.flip_h = sprite_direction < 1
