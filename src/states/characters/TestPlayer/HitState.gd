bda3c53a7240715e53d3ef251ed2d05041c855e5
paruparo <togami.playz2905@gmail.com>
2026-09-16 02:31:03 +0800

    feat: add hit and stunned state to TestPlayer character

---
+++ src/states/characters/TestPlayer/HitState.gd
@@ -0,0 +1,22 @@
+extends State
+
+
+@export var duration : float = 0.1
+
+
+var timer : float
+
+
+func enter(_data: Variant = null) -> void:
+	host.velocity.x = _data.knockback * signi(host.global_position.x - _data.global_position.x)
+	timer = duration
+	
+	
+func physics_update(_delta: float) -> void:
+	if timer > 0:
+		timer -= _delta
+		
+		if timer <= 0:
+			request.emit("Stunned")
+		
+		return
---
+++ src/states/characters/TestPlayer/StunnedState.gd
@@ -0,0 +1,22 @@
+extends State
+
+
+@export var duration : float = 0.1
+
+
+var timer : float
+
+
+func enter(_data: Variant = null) -> void:
+	host.velocity.x = 0
+	timer = duration
+
+
+func physics_update(_delta: float) -> void:
+	if timer > 0:
+		timer -= _delta
+
+		if timer <= 0:
+			request.emit("Idle")
+
+		return
