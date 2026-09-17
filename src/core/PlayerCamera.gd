e0a3e2312d7c3379d25d7b4e35005f27cd7f8bfe
paruparo <togami.playz2905@gmail.com>
2026-09-17 12:46:03 +0800

    feat: add player camera with smooth movement

---
+++ src/core/PlayerCamera.gd
@@ -0,0 +1,22 @@
+extends Camera2D
+
+class_name PlayerCamera
+
+
+@export var target : Node2D
+@export var speed : float
+
+
+func _ready() -> void:
+	global_position = target.global_position
+
+
+func _physics_process(_delta: float) -> void:
+	var target_pos : Vector2
+	if Input.is_action_pressed("LookDown"):
+		target_pos = target.global_position
+		target_pos.y += 100
+	else:
+		target_pos = target.global_position
+	
+	global_position = global_position.slerp(target_pos, 0.1)
