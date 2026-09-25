7357e463ef90ec09fe44369f75adb9e3d6ece3a0
paruparo <togami.playz2905@gmail.com>
2026-09-25 13:34:17 +0800

    feat: enhance SkillsHUD and SkillSlot with cooldown visualization
    - renamed `SkillsHud` to `SkillsHUD` for consistency
    - added `CooldownPanel` and `CDTimerLabel` to `SkillSlot` for displaying cooldowns
    - implemented binding and unbinding logic in `SkillSlot` to manage cooldown updates
    - connected `skill.cooldown_changed` signal to dynamically update cooldown indicators
    - ensured proper reset of `SkillSlot` state during unbind operations

--- src/ui/SkillSlot.gd
+++ src/ui/SkillSlot.gd
@@ -3,10 +3,31 @@ extends PanelContainer
 class_name SkillSlot
 
 
-var data : SkillData
+var skill : Skill
 
 
 @onready var icon : TextureRect = $Icon
+@onready var cooldown_panel : PanelContainer = $CooldownPanel
+@onready var cd_timer : Label = $CooldownPanel/CDTimerLabel
+
+
+func bind(_skill: Skill) -> void:
+	skill = _skill
+	
+	cooldown_panel.visible = false
+	
+	if not skill.cooldown_changed.is_connected(_on_skill_cooldown_changed):
+		skill.cooldown_changed.connect(_on_skill_cooldown_changed)
+		
+		
+func unbind() -> void:
+	if skill == null:
+		return
+
+	cooldown_panel.visible = false
+	
+	if skill.cooldown_changed.is_connected(_on_skill_cooldown_changed):
+		skill.cooldown_changed.disconnect(_on_skill_cooldown_changed)
 
 
 func update_visual(_data: SkillData) -> void:
@@ -17,3 +38,15 @@ func update_visual(_data: SkillData) -> void:
 		return
 	
 	icon.texture = _data.icon
+	
+	
+func _on_skill_cooldown_changed(remaining: float) -> void:
+	if remaining <= 0:
+		cd_timer.text = ""
+		cooldown_panel.visible = false
+		return
+
+	if not cooldown_panel.visible:
+		cooldown_panel.visible = true
+	
+	cd_timer.text = "%.1f" % remaining
--- src/ui/SkillsHud.gd
+++ src/ui/SkillsHud.gd
@@ -1,7 +1,6 @@
 extends HBoxContainer
 
-
-class_name SkillsHud
+class_name SkillsHUD
 
 
 var skill_slot_scene : PackedScene = preload("uid://d343aywka0ysw")
@@ -11,22 +10,25 @@ var slots : Array[SkillSlot]
 func bind(skills: SkillsComponent) -> void:
 	# remove this later and use independent player and UI binding class
 	await ready
+
+	skills.equipped_changed.connect(_update_slots)
 	
 	for i in range(3):
 		var instance : SkillSlot = skill_slot_scene.instantiate()
 		add_child(instance)
 		slots.append(instance)
-			
-	skills.equipped_changed.connect(_update_slots)
 
 	_update_slots(skills.list, 0)
 		
 		
 func _update_slots(skills: Array[Skill], index: int) -> void:
 	if skills.size() <= 1:
+		slots[1].bind(skills[0])
 		slots[1].update_visual(skills[0].data)
 		return
 	
 	for i in range(slots.size()):
 		var skill := skills[(index + i - 1) % skills.size()]
+		slots[i].unbind()
+		slots[i].bind(skill)
 		slots[i].update_visual(skill.data)
